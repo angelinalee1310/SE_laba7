6452d38cf76b7e3a29a8cb6a9fe90aa43af80078
kSara29 <kuandykovasara29@gmail.com>
2020-10-19 15:11:08 +0600

    lol

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ let sum = document.getElementById('sum')
 
 
 let error_salary = document.getElementById('error-salary')
+let error_age = document.getElementById('error-age')
 
 
 let flag = true
 
 function Check(){
-    let salary_year = eval(salary_month * 12)
+    let salary_year = Number(salary_month.value) * 12
     console.log(salary_year)
     //Ограничение по минимальному доходу: для физ. лиц 720 000 тенге в год для юр. лиц 3 000 000 тенге в год
     if(status.value=='Физическое лицо' && salary_year<720000){
@@ -31,6 +32,30 @@ function Check(){
     }
     return flag
 }
+
+function Age(){
+    if (status.value == 'Физическое лицо' && age.value<18){
+        flag = false
+        error_age.style.display = 'block'
+        error_age.innerHTML = 'Минимальный возраст для физических лиц - 18 лет'
+    }
+
+    else if (age.value >= 63 && gender.value == 'Мужской'){
+        flag = false
+        error_age.style.display = 'block'
+        error_age.innerHTML = 'Вы достигли пенсионного возраста'
+    }
+    else if (age.value >= 58 && gender.value == 'Женский'){
+        flag = false
+        error_age.style.display = 'block'
+        error_age.innerHTML = 'Вы достигли пенсионного возраста'
+    }
+    return flag
+}
+
+
+
 function Send(){
     Check()
+    Age()
 }
